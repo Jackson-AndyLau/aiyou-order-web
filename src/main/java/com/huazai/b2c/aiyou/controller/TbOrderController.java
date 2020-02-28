0596d3ecfc24a83a450309a36f3986bad63358fd
panda <dl.herbert@foxmail.com>
2020-02-28 10:54:20 +0800

    新增订单详情显示接口

--- src/main/java/com/huazai/b2c/aiyou/controller/TbOrderController.java
+++ src/main/java/com/huazai/b2c/aiyou/controller/TbOrderController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Description;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -40,7 +41,7 @@ public class TbOrderController
 {
 	@Autowired
 	private TbUserService tbUserService;
-	
+
 	@Autowired
 	private TbItemCartService tbItemCartService;
 
@@ -68,14 +69,52 @@ public class TbOrderController
 		}
 		// 展示用户配置过的地址列表，根据UserId获取
 		// TODO
-		
+
 		// 展示订单支付方式（银联、微信、支付宝等），从库中获取配置的支付方式
 		// TODO
-		
+
 		// 从Cookie中获取购物车列表信息
-		return "";
+		List<TbItemCartVO> cookieTbItemCartVOs = this.getTbItemCartByCookie(request);
+		// 获取用户服务器上的购物车列表
+		List<TbItemCartVO> redisTbItemCartVOs = tbItemCartService
+				.queryTbItemCartByUserId(tbUser == null ? -1 : tbUser.getId());
+		// 将用户本地Cookie中的商品数据合并到服务器的购物车中
+		if (!CollectionUtils.isEmpty(cookieTbItemCartVOs) && !CollectionUtils.isEmpty(redisTbItemCartVOs))
+		{
+			// 标识购物车商品是否被更新
+			boolean flag = false;
+			for (TbItemCartVO cookieCartVO : cookieTbItemCartVOs)
+			{
+				for (TbItemCartVO redisCartVO : redisTbItemCartVOs)
+				{
+					// 以服务器为基准，如果用户本地Cookie中的购物车商品与用户服务器上的购物车商品相同，则用户服务器上的购物车上品数量 + Cookie中相同商品的商品数量
+					if (cookieCartVO.getId() == redisCartVO.getId())
+					{
+						redisCartVO.setNum(redisCartVO.getNum() + cookieCartVO.getNum());
+						// 修改用户服务器购物车商品数量
+						tbItemCartService.updateTbItemCartByUserIdAndItemId(tbUser.getId(), redisCartVO.getId(),
+								redisCartVO.getNum());
+						// flag = true 标识已更新
+						flag = true;
+					}
+				}
+				// 如果Cookie中的商品与用户服务器上的商品不匹配，则需要新增到服务器上
+				if (flag == false)
+				{
+					tbItemCartService.addTbItemCart(tbUser.getId(), cookieCartVO, cookieCartVO.getNum());
+				}
+			}
+		}
+		// 数据合并完成，清除用户本地Cookie中的购物车信息
+		if (!CollectionUtils.isEmpty(cookieTbItemCartVOs))
+		{
+			CookieUtils.deleteCookie(request, response, TB_ITEM_CART_LOCAL_KEY);
+		}
+		// 更新View购物车数据
+		request.setAttribute("cartList", redisTbItemCartVOs);
+		return "order-cart";
 	}
-	
+
 	/**
 	 * 
 	 * @author HuaZai
